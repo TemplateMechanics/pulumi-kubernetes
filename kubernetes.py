c4af380f7958b5809c115194a71e1f4d8e0eef38
Dave Johnson <systematic.tech@outlook.com>
2023-06-01 09:11:02 -0700

    fix: revise way naming was being done based on the kubernetes providers

--- kubernetes.py
+++ kubernetes.py
@@ -154,7 +154,7 @@ class Secrets(BaseResource):
         secret_init_args = mapper.to(core.v1.SecretInitArgs).map(args, use_deepcopy=False, skip_none_values=True)
         secret_init_args.metadata = meta.v1.ObjectMetaArgs(
             name=self.context.get_default_resource_name(self.name), 
-            namespace= args.metadata.namespace or "default"
+            namespace= args.metadata.namespace if args.metadata.namespace is not None else self.context.get_default_resource_name(self.name)
             )
         return core.v1.Secret(self.context.get_default_resource_name(self.name), args=secret_init_args)
 
